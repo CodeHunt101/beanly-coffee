7147e6dc5a76031032b2d98b144b102b307dd857
Harold Torres <harold.torres@websilk.com.au>
2024-09-05 12:33:40 +1000

    add typography and some initial variables

---
+++ app/utils/fonts.ts
@@ -0,0 +1,22 @@
+import { Fraunces, Barlow } from "next/font/google";
+
+const frauncesInit = Fraunces({
+  weight: "900",
+  subsets: ["latin"],
+  variable: "--ff-serif",
+  fallback: ["serif"],
+  preload: true,
+  adjustFontFallback: false,
+});
+const barlowInit = Barlow({
+  weight: ["400", "700"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--ff-sans-serif",
+  fallback: ["sans-serif"],
+  preload: true,
+  adjustFontFallback: false,
+});
+
+export const fraunces = frauncesInit.variable;
+export const barlow = barlowInit.variable;
