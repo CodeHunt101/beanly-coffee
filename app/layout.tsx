b30f2ab4410e53c68c9e0d16965b9b4bd88f9f4a
Harold Torres <harold.torres@websilk.com.au>
2024-09-05 12:38:42 +1000

    add initial head metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,22 @@
 import "@/styles/global.scss";
 import { barlow, fraunces } from "./utils/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Beanly",
+  description: "", //TODO: add description
+  // metadataBase: new URL('/') Not needed for vercel
+  authors: [{ name: "Harold Torres Marino" }],
+  creator: "Harold Torres Marino",
+  publisher: "Harold Torres Marino",
+  twitter: {
+    card: "summary_large_image",
+    title: "Beanly",
+    description: "", //TODO: add description
+    creator: "Harold Torres Marino",
+    // images: '{domain}/opengraph-image.png',
+  },
+};
 
 export default function RootLayout({
   children,
