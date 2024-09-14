2bc98e203154f5e5c3fa3440d73daad215af0065
Harold Torres <haroldtm55@gmail.com>
2024-09-14 23:35:34 +1000

    add scrollspy functionallity

--- app/create-plan/_sections/plan/StepsList.tsx
+++ app/create-plan/_sections/plan/StepsList.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import styles from "./CreatePlanSection.module.scss";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PlanContext, PlanContextType } from "@/app/_context/planContext";
 
 const StepList = () => {
   const { selectedOptions } = useContext<PlanContextType>(PlanContext);
+  const [activeStep, setActiveStep] = useState<number | null>(null);
+
   const steps = [
     "Preferences",
     "Bean Type",
@@ -12,6 +14,26 @@ const StepList = () => {
     "Grind Option",
     "Deliveries",
   ];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY + 115; // Adjust for the 115px offset
+      const sections = steps.map((_, index) =>
+        document.getElementById(`question-${index + 1}`),
+      );
+
+      for (let i = sections.length - 1; i >= 0; i--) {
+        const section = sections[i];
+        if (section && section.offsetTop <= scrollPosition) {
+          setActiveStep(i);
+          break;
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <ol className={`${styles.stepList} ff-serif`}>
@@ -25,7 +47,11 @@ const StepList = () => {
             >
               0{index + 1}
             </span>
-            <span className={styles.step}>{step}</span>
+            <span
+              className={`${styles.step} ${activeStep === index ? styles.active : ""}`}
+            >
+              {step}
+            </span>
           </Link>
         </li>
       ))}
